78354cf8195a49ae0d3e119df4d3ef0a5a3a4aa6
LaurentDIA16 <dia16@polenumerique.re>
2022-11-18 20:30:17 +0400

    Ajout de commentaires dans fichier scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,128 +3,138 @@
 let wordEn;
 let wordFr;
 
- // Création d'une image
+
+document.addEventListener('DOMContentLoaded', () => {
+  handleDrop()
+})
+
+// Création de la balise img
 let img_to_detec = document.createElement('img');
 
+// Ici qu'on lance la detection automatique lorsque l'image est droper dans le cadre 
+img_to_detec.addEventListener('load', () => {
+    console.log('ready: ', img_to_detec);
 
- // Ici qu'on lance  la detection automatique lorsque l'image est droper dans le cadre 
- img_to_detec.addEventListener('load', () => {
-    console.log('okok: ', img_to_detec);
+    //Chargement du modèle pré-entrainer cocossd
     detector = ml5.objectDetector('cocossd');
-    // detectObjAffichage(img_to_detec)
- })
+})
 
- // Fonction pour écouter le drop d'image
- function handleDrop(){
+// Fonction pour écouter le drop d'image
+function handleDrop(){
     const image_drop_area = document.querySelector("#image_drop_area");
-    const detecte_obj_btn = document.getElementById('detect_obj')
     
+    // création bouton (Lancez la détection), lancement de la fonction detectObjAffichage à son appui
+    const detecte_obj_btn = document.getElementById('detect_obj')
     detecte_obj_btn.addEventListener('click', ()=>{
-      detectObjAffichage(img_to_detec) //detectObj
+      detectObjAffichage(img_to_detec)
     })
 
     image_drop_area.addEventListener('dragover', (event) => {
       event.stopPropagation();
       event.preventDefault();
-      event.dataTransfer.dropEffect = 'copy';
+      event.dataTransfer.dropEffect = 'copy'; //Copie de la donnée glisser vers le lieu de dépôt
+    });
+
+    image_drop_area.addEventListener('drop', (event) => {
+      event.stopPropagation(); //évite que l'évènement se propage plus loin
+      event.preventDefault(); //empêche l'action par défaut de l'évènement
+
+      //Création variable fileList pour récupérer les infos de l'image déposer
+      const fileList = event.dataTransfer.files;
+      
+      //Activation de la fonction ImageReader avec les infos de fileList 
+      ImageReader(fileList[0]);
     });
  
     function ImageReader(file){
+      //Création d'une nouvelle instance de la classe FileReader
       const reader = new FileReader();
       reader.addEventListener('load', (event) => {
-            
-          uploaded_image = event.target.result; 
           
+          //Création de la variable pour récupérer les données source de l'image
+          uploaded_image = event.target.result;
+          
+          //Ajout de l'attribut src(données source de l'image) à la balise img créé plus haut
           document.querySelector("#image_drop_area").style.backgroundImage = `url(${uploaded_image})`;
           img_to_detec.src = uploaded_image
-    
       });
+
+      //Lire le contenu de file et restitué sous la forme d'une URL de donnée ensuite renvoi dans la balise img l'attribut src
       reader.readAsDataURL(file);
     }
- 
-    image_drop_area.addEventListener('drop', (event) => {
-      event.stopPropagation();
-      event.preventDefault();
-      const fileList = event.dataTransfer.files;
-      // document.querySelector("#file_name").textContent = fileList[0].name;
-      ImageReader(fileList[0]);
-      // detectObj(img_to_detec)
-    });
-  }
+}
 
+// fonction pour identifier les objets sur l'image et proposer la traduction
 function detectObjAffichage(image){
-  // detector = ml5.objectDetector('cocossd');
+  // Lancement de l'identification de l'objet
   detector.detect(image,function(error, result){
     if(error) {
       console.error(error);
       return;
     }
-    //console.log(result);
+
+    // Récupère le label (nom) de l'objet détecter et la précision (confidence)
     wordEn = result[0].label;
     let confidence = result[0].confidence;
 
+    // Boucle dans le dictionnaire fr-en
     for(let key in dico){
       if (key == wordEn){ //si la clé est égale au mot à traduire
         wordFr = dico[key]; //créé une variable avec le mot traduit
-        document.querySelector("#obj_detect").textContent = wordFr; //ajouter le mot fr sur la page HTML
+        document.querySelector("#obj_detect").textContent = wordFr; //ajouter le mot fr sur la page HTML id obj_detect
+      }
+    }
+    // Calcul de la précision en % et ajout dans l'id obj_confidence 
+    document.querySelector("#obj_confidence").textContent = 100 * confidence.toFixed(2) + " %";
+  });
+  
+  //Création bouton (Lancez la traduction)
+  let elementsButton = document.getElementsByTagName("button"); 
+  let buttonTr = elementsButton[6];
+  
+  buttonTr.addEventListener('click', () => {
+    document.getElementById("wordEn").innerHTML = wordEn;
+
+    //Créer une variable qui récupère le mot en anglais dans id wordEn pour futur traduction en français
+    wordToTrad = document.getElementById("wordEn").innerText;
+
+    //boucle dans le dictionnaire "dico" pour retrouver le mot à traduire en-fr
+    for(let key in dico){
+      if (key == wordToTrad){ //si la clé est égale au mot à traduire
+        let newWordFr = dico[key]; //créé une variable avec le mot traduit
+        document.getElementById("wordFr").innerHTML = newWordFr; //ajouter le mot fr sur la page HTML id wordFr
       }
     }
 
-    document.querySelector("#obj_confidence").textContent = 100 * confidence.toFixed(2) + " %";    //confidence.toFixed(2)
+    //Récupérer le mot français en string
+    wordFr = document.getElementById("wordFr").innerText;
+    console.log(wordFr);
+
+    //Phrase d'intro pour le mot français
+    const lectureTrFr = new SpeechSynthesisUtterance("En français ca se dit");
+    lectureTrFr.volume = 1;
+    lectureTrFr.lang = 'fr-ca';;
+    speechSynthesis.speak(lectureTrFr);
+
+    //Retranscription du mot fr en parole avec accent fr
+    const lectureFr = new SpeechSynthesisUtterance(wordFr);
+    lectureFr.volume = 1;
+    lectureFr.lang = 'fr-ca';;
+    speechSynthesis.speak(lectureFr);
+
+    //Phrase d'intro pour le mot anglais
+    const lectureTrEn = new SpeechSynthesisUtterance("Et en anglais");
+    lectureTrEn.volume = 1;
+    lectureTrEn.lang = 'fr-ca';;
+    speechSynthesis.speak(lectureTrEn);
+
+    //Class qui permet de retranscrire du texte en parole
+    const lectureEn = new SpeechSynthesisUtterance(wordEn);
+    lectureEn.volume = 1;
+    lectureEn.lang = 'en-en';
+    speechSynthesis.speak(lectureEn);
   });
-  
-  let elementsButton = document.getElementsByTagName("button"); 
-    let buttonTr = elementsButton[6]; //Bouton TRADUCTION
-  
-    buttonTr.addEventListener('click', () => {
-      document.getElementById("wordEn").innerHTML = wordEn;
-      console.log(wordEn);
-
-      //Créer une variable qui récupère le mot en anglais pour futur traduction en français
-      wordToTrad = document.getElementById("wordEn").innerText;
-
-      //boucle dans le dictionnaire "dico" pour retrouver le mot à traduire en-fr
-      for(let key in dico){
-          if (key == wordToTrad){ //si la clé est égale au mot à traduire
-            let newWordFr = dico[key]; //créé une variable avec le mot traduit
-            document.getElementById("wordFr").innerHTML = newWordFr; //ajouter le mot fr sur la page HTML
-          }
-      }
-  
-      //Récupérer le mot français en string
-      wordFr = document.getElementById("wordFr").innerText;
-      console.log(wordFr);
-
-      //Phrase d'intro pour le mot français
-      const lectureTrFr = new SpeechSynthesisUtterance("En français ca se dit");
-      lectureTrFr.volume = 1;
-      lectureTrFr.lang = 'fr-ca';;
-      speechSynthesis.speak(lectureTrFr);
-
-      //Retranscription du mot fr en parole avec accent fr
-      const lectureFr = new SpeechSynthesisUtterance(wordFr);
-      lectureFr.volume = 1;
-      lectureFr.lang = 'fr-ca';;
-      speechSynthesis.speak(lectureFr);
-
-      //Phrase d'intro pour le mot anglais
-      const lectureTrEn = new SpeechSynthesisUtterance("Et en anglais");
-      lectureTrEn.volume = 1;
-      lectureTrEn.lang = 'fr-ca';;
-      speechSynthesis.speak(lectureTrEn);
-
-      //Class qui permet de retranscrire du texte en parole
-      const lectureEn = new SpeechSynthesisUtterance(wordEn);
-      lectureEn.volume = 1;
-      lectureEn.lang = 'en-en';
-      speechSynthesis.speak(lectureEn);
-
-    });
 };
-
-document.addEventListener('DOMContentLoaded', () => {
-    handleDrop()
-})
 
 //dictionnaire des catégories du modèle et leurs traduction en-fr
 const dico = {
